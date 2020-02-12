4ed6fefe48a88f86196dfc0703a6fbb92a699e2e
Chris Hundhausen <chundhau@gmail.com>
2020-02-12 09:12:31 -0800

    Updated handlechange; added computeSGS

--- scripts/roundForm.js
+++ scripts/roundForm.js
@@ -6,12 +6,28 @@ class RoundForm extends React.Component {
       //store date as ISO string
       this.state = {date:  today.toISOString().substr(0,10), 
                     course: "",
-                    type: "practice"}; 
+                    type: "practice",
+                    holes: "18",
+                    strokes: 80,
+                    minutes: 50,
+                    seconds: "00",
+                    notes: ""};  
     }
   
+    computeSGS = () => {
+      return (Number(this.state.strokes) + Number(this.state.minutes)) 
+                  + ":" + this.state.seconds;
+    }
+
     handleChange = (event) => {
       const name = event.target.name;
-      this.setState({[name]: event.target.value});
+      if (name === "seconds") {
+        this.setState({seconds: 
+          (event.target.value.length < 2 ? "0" + event.target.value : 
+             event.target.value)});
+      } else {
+        this.setState({[name]: event.target.value});
+      }
     }
   
     handleSubmit = (event) => {
@@ -44,7 +60,42 @@ class RoundForm extends React.Component {
           </select> 
           </label>
           <p></p>
-          <input type="submit" className="btn btn-primary" value="Submit" />
+          <label># Holes:
+          <select name="holes" value={this.state.holes} 
+            className="form-control form-center" onChange={this.handleChange}>
+            <option value="9">9</option>
+            <option value="18">18</option>
+          </select> 
+          </label>
+          <p></p>
+          <label># Strokes:
+          <input name="strokes" className="form-control form-center" type="number" 
+            min="9" max="200" value={this.state.strokes} 
+            onChange={this.handleChange} />
+          </label>
+          <p></p>
+          <label>Time: <br></br>
+          <input name="minutes" type="number" size="3"
+            min="10" max="400" value={this.state.minutes}
+            onChange={this.handleChange} />:  
+          <input name="seconds" type="number" size="2"
+            min="0" max="60" value={this.state.seconds} 
+            onChange={this.handleChange} />
+          </label>
+          <p></p>
+          <label>Speedgolf Score: <br></br>
+              <input name="SGS" className="form-center" type="text" size="6" 
+                disabled={true} value={this.computeSGS()} />
+          </label>
+          <p></p>
+          <label>Notes:
+              <textarea name="notes" className="form-control" rows="6" cols="75" 
+                placeholder="Enter round notes" value={this.state.notes} 
+                onChange={this.handleChange} />
+          </label>
+          <p></p>
+          <input type="submit" style={{width: "70%",fontSize: "36px"}} 
+            className="btn btn-primary btn-color-theme" value="Save Round Data" />
           </center>
         </form>
       );
